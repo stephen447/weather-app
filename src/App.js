9d1d0f6545a514c7547eaa268a09865c19f2c423
Stephen Byrne <sdavidbyrne@gmail.com>
2023-06-21 21:01:44 +0100

    Functioning - no CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React, {useState, useEffect} from "react";
+import Button from '@mui/material/Button';  
 import ReactAnimatedWeather from 'react-animated-weather';
 
 function App() {
-  const [location, setLocation] = useState("")
-  const [data, setData] = useState('')
-  const [localdata, setLocaldata] = useState("")
+  const [location, setLocation] = useState(undefined)
+  const [currentloc, setCurrentloc] = useState(true)
+  const [data, setData] = useState(undefined)
   const [longitude, setLongitude] = useState(undefined)
   const [latitude, setLatitude] = useState(undefined)
 
@@ -21,25 +22,31 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     let apikey = '6e7e45fd86e60c1ed32d3373a9b51508'
-
-    if(location !==""){
-      fetch(`http://api.openweathermap.org/data/2.5/weather?id=524901&q=${location},IE&units=metric&appid=${apikey}`)
+    if (currentloc===true){
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apikey}`)
       .then(response => response.json())
       .then(data =>{setData(data)})
     }
     else{
-      
-
+      fetch(`http://api.openweathermap.org/data/2.5/weather?id=524901&q=${location},IE&units=metric&appid=${apikey}`)
+      .then(response => response.json())
+      .then(data =>{setData(data)})
     }
   }
 
-  function getlocaldata(){
-
-    let apikey = '6e7e45fd86e60c1ed32d3373a9b51508'
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apikey}`)
+  const handleclick = (e) =>{
+    setCurrentloc((current) => !current);
+    if(currentloc===false){
+      let apikey = '6e7e45fd86e60c1ed32d3373a9b51508'
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apikey}`)
       .then(response => response.json())
-      .then(data =>{setLocaldata(data)})
+      .then(data =>{setData(data)})
+      
+    }
+    
+    console.log(data)
   }
+
 
   function getIcon(weather){
     switch(weather){
@@ -87,67 +94,51 @@ function App() {
     }
   }
   useEffect(componentDidMount)
-  useEffect(getlocaldata)
   return (
     <div className="App">
-      <div className='form'>
-        <label>Enter your location:
-          <input type="text" value={location} onChange={(e) => setLocation(e.target.value)}/>
-          <input type="submit" onClick={handleSubmit}/>
-        </label>
+      <Button onClick={handleclick}>Hello </Button>
+      {currentloc===true?(
+        <p> Weather for your current location</p>
+      ):(
+        <div className='form'>
+          <label>Enter your location:
+            <input type="text" value={location} onChange={(e) => setLocation(e.target.value)}/>
+            <input type="submit" onClick={handleSubmit}/>
+          </label>
+        </div>
+      )
+      }
+      
+      {typeof data ==='undefined' ? (
+      <div>
+        <p>Welcome to the weather app!</p>
+      </div>
+      ) : (
+      <div className='city'>
+        <div className="city">
+        <h1>{data.name}</h1>
       </div>
 
-        {data ==='' && localdata==='' ?(
-          
-        <div className='city'>
-          <div className="city">
-            <h1>{localdata.name}</h1>
+      <div className="weather-info">
+        <div className="container">
+          <div className="column">
+            <p> Current Temperature: {data.main.temp} Celcius</p>
+            <p> Maximum temperature: {data.main.temp_max} Celcius</p>
+            <p> Minimum temperature: {data.main.temp_min} Celcius</p>    
           </div>
-
-        <div className="weather-info">
-          <div className="container">
-            <div className="column">
-              <p> Current Temperature: {localdata.main.temp} Celcius</p>
-              <p> Maximum temperature: {localdata.main.temp_max} Celcius</p>
-              <p> Minimum temperature: {localdata.main.temp_min} Celcius</p>          
-            </div>
-            <div className='column'>
-              <p> Feels like: {localdata.main.feels_like}</p>      
-              <p>{localdata.weather[0].main}</p> 
-            </div>
-          </div>
-          <div className='icon'>
-            <ReactAnimatedWeather icon={getIcon(localdata.weather[0].main)} color={getIconColor(localdata.weather[0].main)} size={200} animate={true}/>
+          <div className='column'>
+            <p> Feels like: {data.main.feels_like}</p>      
+            <p>{data.weather[0].main}</p> 
           </div>
         </div>
+        <div className='icon'>
+          <ReactAnimatedWeather icon={getIcon(data.weather[0].main)} color={getIconColor(data.weather[0].main)} size={200} animate={true}/>
         </div>
-        ) : (
-        <div className='city'>
-          <div className="city">
-          <h1>{data.name}</h1>
-        </div>
-
-        <div className="weather-info">
-          <div className="container">
-            <div className="column">
-              <p> Current Temperature: {data.main.temp} Celcius</p>
-              <p> Maximum temperature: {data.main.temp_max} Celcius</p>
-              <p> Minimum temperature: {data.main.temp_min} Celcius</p>    
-                    
-            </div>
-            <div className='column'>
-              <p> Feels like: {data.main.feels_like}</p>      
-              <p>{data.weather[0].main}</p> 
-            </div>
-          </div>
-          <div className='icon'>
-            <ReactAnimatedWeather icon={getIcon(data.weather[0].main)} color={getIconColor(data.weather[0].main)} size={200} animate={true}/>
-          </div>
-        </div>
-        </div>
-        )
-        }
+      </div>
     </div>
+      )}
+  </div>
+        
   );
 
 }
